765e2cc7633e38e68b262baeaa80728606720fbb
bmorrell <bmorrell@brightcove.com>
2015-12-21 17:18:13 +0800

    removing zencoder

--- BenAkamai/ingest-dashboard/clear-log.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $logFileLocation = "video-ids.js";
-    $freshContent = "var videoIdArray = [];";
-    file_put_contents($logFileLocation, $freshContent);
-
-echo 'Log file cleared - <a href="di-log.html">go back to the dashboard</a>';
-?>
